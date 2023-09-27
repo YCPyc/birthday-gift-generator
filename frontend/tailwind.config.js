8c47c72e113c7e6a49cd589af9acef2d43f336f3
Jun Park <jupark@yext.com>
2023-09-27 00:50:37 -0400

    feat(frontend): styling updates, and adding react-router/error screen

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "black",
+        "brand-primary": "#1B78D0",
+        "brand-secondary": "#073866",
+        "brand-skyblue": "#4b7b8b",
+        "brand-gray": {
+          100: "#F6F6F6",
+          200: "#C5C5C5",
+          300: "#A6AEBB",
+        },
+      },
+    },
   },
   plugins: [],
 }
