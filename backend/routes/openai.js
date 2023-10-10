d1097e75912eade51f929462cf388c980415e815
YC Pan <yp9ar@virginia.edu>
2023-10-10 02:12:27 -0400

    updated prompt

--- backend/routes/openai.js
+++ backend/routes/openai.js
@@ -7,14 +7,14 @@ const openai = new OpenAI({
     apiKey:  process.env["OPEN_AI_KEY"]
   });
 
-router.post("/", async function(req,res,next){
+router.post("/askideas", async function(req,res,next){
     try{
         const{race, hobbies, gender} = req.body;
         const chatCompletion = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: [
               {role: "system", content: "You are a helpful advisor on gift ideas."},
-              {role: "user", content: "My friend's ethnicity is"+race+". These are their hobbbies: "+hobbies+". This is their genders:"+gender+". Can you give me 3 gift ideas for them and structure the ideas in a json object."}],
+              {role: "user", content: "My friend's ethnicity is"+race+". These are their hobbbies: "+hobbies+". This is their genders:"+gender+". Can you give me 3 gift ideas for them and structure the ideas in a json object. Please only reply with a json object"}],
           });
         return res.status(200).json({
             result:chatCompletion.choices[0]['message']['content']
